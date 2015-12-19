f109d37d0a8a150632dae82cce80a8128e39bb1d
mehtab <mehtab.ghani@gmail.com>
2015-12-19 15:23:34 +0500

    getters setters created.

--- app/src/main/java/com/bathem/vocabpower/Entity/Vocab.java
+++ app/src/main/java/com/bathem/vocabpower/Entity/Vocab.java
@@ -12,7 +12,27 @@ public class Vocab {
     private ArrayList<String> examples;
 
 
+    public String getVocab() {
+        return vocab;
+    }
 
+    public void setVocab(String vocab) {
+        this.vocab = vocab;
+    }
 
+    public ArrayList<String> getMeaning() {
+        return meaning;
+    }
 
+    public void setMeaning(ArrayList<String> meaning) {
+        this.meaning = meaning;
+    }
+
+    public ArrayList<String> getExamples() {
+        return examples;
+    }
+
+    public void setExamples(ArrayList<String> examples) {
+        this.examples = examples;
+    }
 }
