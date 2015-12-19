204b889096d1b2e71c7ab329391af4c7fbdacc67
mehtab <mehtab.ghani@gmail.com>
2015-12-19 15:07:08 +0500

    Initial project setup

---
+++ app/src/main/java/com/bathem/vocabpower/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bathem.vocabpower.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bathem.vocabpower.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/bathem/vocabpower/Entity/Vocab.java
@@ -0,0 +1,18 @@
+package com.bathem.vocabpower.Entity;
+
+import java.util.ArrayList;
+
+/**
+ * Created by mehtab on 12/19/15.
+ */
+public class Vocab {
+
+    private String vocab;
+    private ArrayList<String> meaning;
+    private ArrayList<String> examples;
+
+
+
+
+
+}
