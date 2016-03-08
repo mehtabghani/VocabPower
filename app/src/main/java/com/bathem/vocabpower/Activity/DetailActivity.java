ec69d98b42b44b386485e3d0ca55ff04d799cb6f
Mehtab <mehtab@local.local>
2016-03-08 16:41:43 +0500

    add button image set.

--- app/src/main/java/com/bathem/vocabpower/Activity/DetailActivity.java
+++ app/src/main/java/com/bathem/vocabpower/Activity/DetailActivity.java
@@ -3,6 +3,8 @@ package com.bathem.vocabpower.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.bathem.vocabpower.R;
+
 public class DetailActivity extends AppCompatActivity {
 
     @Override
