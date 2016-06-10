8e705028e1eaa6a35c97d8b183b6e978156befae
Mehtab <mehtab@local.local>
2016-06-10 16:35:02 +0500

    Shared preferences helper added.

---
+++ app/src/main/java/com/bathem/vocabpower/Application/AppManager.java
@@ -0,0 +1,27 @@
+package com.bathem.vocabpower.Application;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by mehtab on 10/06/16.
+ */
+public class AppManager extends Application {
+
+
+    static AppManager sInstance;
+    Context mApplicationContext;
+
+    public static AppManager getsInstance() {
+        if(sInstance ==  null)
+            sInstance = new AppManager();
+
+        return sInstance;
+    }
+
+
+    public Context getApplicationContext() {
+
+        return getApplicationContext();
+    }
+}
---
+++ app/src/main/java/com/bathem/vocabpower/Helper/SharedPreferenceHelper.java
@@ -0,0 +1,48 @@
+package com.bathem.vocabpower.Helper;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.bathem.vocabpower.Application.AppManager;
+
+/**
+ * Created by mehtab on 10/06/16.
+ */
+public class SharedPreferenceHelper {
+
+    static SharedPreferenceHelper sInstance;
+    final static String DEFAULT_SHARED_PREFERNECE = "default_shared_preference";
+
+    public static SharedPreferenceHelper getsInstance() {
+        if(sInstance ==  null)
+            sInstance = new SharedPreferenceHelper();
+
+        return sInstance;
+    }
+
+
+    public String getSharedPreferenceByKey(String key) {
+
+        String result = "";
+        Context context = AppManager.getsInstance().getApplicationContext();
+        SharedPreferences sharedPref =context.getSharedPreferences(DEFAULT_SHARED_PREFERNECE, Context.MODE_PRIVATE);
+
+        if (sharedPref.contains(key))
+            result = sharedPref.getString(key, "");
+
+        return result;
+    }
+
+
+    public void setSharedPreferenceByKey(String key, String value) {
+
+        Context context = AppManager.getsInstance().getApplicationContext();
+        SharedPreferences sharedPref =context.getSharedPreferences(DEFAULT_SHARED_PREFERNECE, Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(key, value);
+        editor.commit();
+
+    }
+
+}
